ea5a52b0be5f5ddbb235389ea74b95c01a872ab7
Oleg Tihonenko <tihonenko.oleg@gmail.com>
2022-12-06 09:46:20 +0300

    add custom select styles

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -14,6 +14,38 @@ export const CustomSelect = () => {
       isSearchable={false}
       value={currentCurrency}
       onChange={handleSelect}
+      styles={{
+        control: (baseStyles, state) => ({
+          ...baseStyles,
+          boxSizing: "border-box",
+          minHeight: 10,
+          padding: 0,
+          fontSize: 12,
+          borderColor: state.isFocused ? "blue" : "gray",
+        }),
+        valueContainer: (baseStyles, state) => ({
+          ...baseStyles,
+          padding: 2,
+        }),
+        indicatorSeparator: (baseStyles, state) => ({
+          ...baseStyles,
+          padding: 0,
+        }),
+        dropdownIndicator: (baseStyles, state) => ({
+          ...baseStyles,
+          padding: 2,
+        }),
+        menuList: (baseStyles, state) => ({
+          ...baseStyles,
+          fontSize: 10,
+          padding: 2,
+          borderRadius: 10,
+        }),
+        menu: (baseStyles, state) => ({
+          ...baseStyles,
+          top: 20,
+        }),
+      }}
     />
   );
 };
---
+++ src/components/CustomSelect/styles.ts
@@ -0,0 +1,14 @@
+
+import styled from "styled-components";
+import { useState } from "react";
+
+export const StyledSelect = styled.select`
+  display: inline-block;
+  width: 160px;
+  height: 34px;
+  line-height: 30px;
+  position: relative;
+  
+  `
+
+
